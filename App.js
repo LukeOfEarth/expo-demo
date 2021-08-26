7dc524f5b6c0e95ec45fc84fb088eceac52a90cb
Luke van Aardt <lukev@bbd.co.za>
2021-08-26 16:28:50 +0200

    post-bootcamp code

--- App.js
+++ App.js
@@ -1,15 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import CustomComponent from './components/custome-component';
-import Effect from './components/effect-demo';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './screens/HomeScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import Footer from './Footer';
+
+import { navigationRef } from './RootNavigation';
+const Stack = createStackNavigator();
 
 export default function App() {
+
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Effect />
-    </View>
+    <NavigationContainer ref={navigationRef}>
+      <Stack.Navigator
+        initialRouteName="home"
+      >
+        <Stack.Screen
+          name="welcome"
+          component={WelcomeScreen}
+        />
+        <Stack.Screen
+          name="home"
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+      <Footer></Footer>
+    </NavigationContainer>
   );
 }
 
@@ -20,4 +41,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  vsop: {
+    color: '#f5e'
+  }
 });
